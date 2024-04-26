0421aebcebb6fcc17b0792f20af3410e40ac99b9
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-26 21:53:13 +0700

    added function show menu in navbar

--- src/components/Elements/Navbar/index.jsx
+++ src/components/Elements/Navbar/index.jsx
@@ -1,12 +1,20 @@
-import Button from '../Button';
+import { useState } from 'react';
+import { CgClose } from 'react-icons/cg';
+import { LiaBarsSolid } from 'react-icons/lia';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+   const [showMenu, setShowMenu] = useState(false);
    const navList = [
       { name: 'Home', link: '' },
       { name: 'Promo', link: '' },
       { name: 'Activity', link: '' },
       { name: 'Dasboard', link: '' },
    ];
+
+   const toggleButton = () => {
+      setShowMenu(!showMenu);
+   };
 
    return (
       <nav className='bg-slate-100 py-2 fixed w-full shadow-[0_0_3px_0] z-[1000]'>
@@ -29,17 +37,44 @@ export default function Navbar() {
                   </div>
                </div>
 
-               <div className='flex gap-2 font-semibold'>
-                  <Button
-                     value={'Login'}
-                     className='bg-green-500 hover:bg-green-600'
-                  />
-                  <Button
-                     value={'Register'}
-                     className='bg-yellow-400 hover:bg-yellow-500'
-                  />
+               <div className='hidden md:block'>
+                  <div className='flex gap-2 font-semibold text-sm'>
+                     <Link className='bg-green-500 hover:bg-green-600 text-white py-1 px-3 rounded-full'>Login</Link>
+                     <Link className='bg-yellow-400 hover:bg-yellow-500 text-white py-1 px-3 rounded-full'>Register</Link>
+                  </div>
                </div>
+               <button
+                  className='md:hidden'
+                  onClick={toggleButton}
+               >
+                  <svg
+                     className='h-6 w-10'
+                     fill='none'
+                     viewBox='0 0 24 24'
+                     stroke='currentColor'
+                  >
+                     {showMenu ? <CgClose size={25} /> : <LiaBarsSolid size={25} />}
+                  </svg>
+               </button>
             </div>
+            {showMenu && (
+               <div className='md:hidden'>
+                  <div className='flex flex-col font-semibold text-sm'>
+                     {navList.map((item, index) => (
+                        <div
+                           key={index}
+                           className='hover:bg-gray-200 ease-in-out duration-200 py-1 px-3 rounded-full cursor-pointer'
+                        >
+                           {item.name}
+                        </div>
+                     ))}
+                  </div>
+                  <div className='flex gap-2 font-semibold text-sm py-1'>
+                     <Link className='bg-green-500 hover:bg-green-600 text-white py-1 px-3 rounded-full'>Login</Link>
+                     <Link className='bg-yellow-400 hover:bg-yellow-500 text-white py-1 px-3 rounded-full'>Register</Link>
+                  </div>
+               </div>
+            )}
          </div>
       </nav>
    );
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,6 @@
-import Navbar from '../../components/Elements/Navbar';
-
 export default function LoginPage() {
    return (
       <div>
-         <Navbar />
          <h1>Login Page</h1>
       </div>
    );
