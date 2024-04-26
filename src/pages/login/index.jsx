5cad71073999605a0bcb8673a9c5a1724faafc72
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-26 22:05:56 +0700

    added layout and place content center in login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,12 @@
+import PlaceContentCenter from '../../components/Elements/PlaceContentCenter';
+import Layout from '../../components/Layouts/Layout';
+
 export default function LoginPage() {
    return (
-      <div>
-         <h1>Login Page</h1>
-      </div>
+      <Layout>
+         <PlaceContentCenter>
+            <h1>Login Page</h1>
+         </PlaceContentCenter>
+      </Layout>
    );
 }
