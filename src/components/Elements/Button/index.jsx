ed5e2751b12989705aeb0d8b0b5a3d79fd6e6eef
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-27 21:59:41 +0700

    added type defautl and added style in button

--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,10 +1,10 @@
-export default function Button({ children, type, value, className = 'bg-black', ...props }) {
+export default function Button({ children, type = 'sumbit', value, className = 'bg-black', ...props }) {
    return (
       <>
          <button
             {...props}
             type={type}
-            className={`${className} text-white py-1 px-3 rounded-full`}
+            className={`${className} text-white py-1 px-3 rounded-full font-semibold tracking-widest`}
          >
             {children || value}
          </button>
