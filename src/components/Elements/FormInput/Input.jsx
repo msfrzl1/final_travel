0a4e11fb349f8b4d83072d8b517ed5f4f45fd17d
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-27 23:52:39 +0700

    added auto complete

--- src/components/Elements/FormInput/Input.jsx
+++ src/components/Elements/FormInput/Input.jsx
@@ -2,6 +2,7 @@ export default function Input({ type = 'text', ...props }) {
    return (
       <input
          {...props}
+         autoComplete='on'
          type={type}
          className='border py-1.5 px-2 rounded-md font-semibold text-sm tracking-normal'
       />
