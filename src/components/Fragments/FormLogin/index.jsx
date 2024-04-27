ed439716a9657ff46fc6136a60c8b414cab16e2e
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-27 23:53:08 +0700

    change htmlFor

--- src/components/Fragments/FormLogin/index.jsx
+++ src/components/Fragments/FormLogin/index.jsx
@@ -3,22 +3,30 @@ import Button from '../../Elements/Button';
 import FormInput from '../../Elements/FormInput';
 
 export default function FormLogin() {
+   const handleSumbit = (e) => {
+      e.preventDefault();
+      const userData = {
+         email: e.target.email.value,
+         password: e.target.password.value,
+      };
+      console.log(userData);
+   };
    return (
       <div className='max-w-lg flex flex-col mx-auto border shadow-[0_0_5px_0] rounded-md p-3 overflow-hidden'>
          <div className='text-center mb-5'>
             <div className='text-2xl font-mono font-black tracking-widest'>Selamat datang!</div>
             <div className='text-sm font-semibold'>Masuk sekarang untuk melanjutkan</div>
          </div>
-         <form>
+         <form onSubmit={handleSumbit}>
             <FormInput
-               htmlfor={'email'}
+               htmlFor={'email'}
                title={'Email'}
                type={'email'}
                name={'email'}
                placeholder={'example123@mail.com'}
             />
             <FormInput
-               htmlfor={'password'}
+               htmlFor={'password'}
                title={'Password'}
                type={'password'}
                name={'password'}
