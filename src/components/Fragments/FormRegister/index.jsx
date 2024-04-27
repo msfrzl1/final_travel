c6f0591cad504508342aed95006f1a2290fd58e1
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-27 21:47:34 +0700

    create form register page

---
+++ src/components/Fragments/FormRegister/index.jsx
@@ -0,0 +1,7 @@
+export default function FormRegister() {
+   return (
+      <div>
+         <h1>Form Register Page</h1>
+      </div>
+   );
+}
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,11 +1,12 @@
 import PlaceContentCenter from '../../components/Elements/PlaceContentCenter';
+import FormRegister from '../../components/Fragments/FormRegister';
 import Layout from '../../components/Layouts/Layout';
 
 export default function RegisterPage() {
    return (
       <Layout>
          <PlaceContentCenter>
-            <h1>Register Page</h1>
+            <FormRegister />
          </PlaceContentCenter>
       </Layout>
    );
