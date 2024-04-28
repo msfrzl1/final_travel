4f35d701b0d7af5e85cc97884b80d2184b8459ee
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-29 00:09:24 +0700

    added data with api banners

--- src/pages/dasboard/banners/bannersPage.jsx
+++ src/pages/dasboard/banners/bannersPage.jsx
@@ -1,9 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { BiEdit } from 'react-icons/bi';
 import { MdDeleteForever, MdOutlineLibraryAdd } from 'react-icons/md';
 import { RiSettings4Line } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import useGetData from '../../../hooks/isGetData';
 
 export default function BannerPage() {
+   const [banners, setBanners] = useState([]);
+   console.log(banners);
+   const { getData } = useGetData();
+
+   useEffect(() => {
+      getData('banners', setBanners);
+   }, []);
+
    return (
       <div>
          <div className='flex-col text-center md:flex md:flex-row items-center justify-between pb-2 mb-3 border-b-2'>
@@ -26,30 +37,33 @@ export default function BannerPage() {
             </Link>
          </div>
          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-            <div
-               className='relative border m-2 rounded-lg overflow-hidden shadow-[0_0_5px_0] transition-transform duration-300 transform hover:scale-[1.01]'
-               data-aos='zoom-in'
-               data-aos-duration='1000'
-            >
-               <img
-                  src=''
-                  alt='banner'
-                  className='w-full h-40 object-cover'
-               />
-               <div className='absolute bottom-0 left-0 right-0 bg-black text-white px-2 py-1'>
-                  <h1 className='text-xs font-semibold'>sas</h1>
-               </div>
-               <Link to=''>
-                  <BiEdit
-                     className='absolute bg-white/95 top-2 right-2 text-indigo-700 hover:text-indigo-800 border rounded-xl'
+            {banners.map((banner) => (
+               <div
+                  key={banner.id}
+                  className='relative border m-2 rounded-lg overflow-hidden shadow-[0_0_5px_0] transition-transform duration-300 transform hover:scale-[1.01]'
+                  data-aos='zoom-in'
+                  data-aos-duration='1000'
+               >
+                  <img
+                     src={banner.imageUrl}
+                     alt='banner'
+                     className='w-full h-40 object-cover'
+                  />
+                  <div className='absolute bottom-0 left-0 right-0 bg-black text-white px-2 py-1'>
+                     <h1 className='text-xs font-semibold'>{banner.name}</h1>
+                  </div>
+                  <Link to=''>
+                     <BiEdit
+                        className='absolute bg-white/95 top-2 right-2 text-indigo-700 hover:text-indigo-800 border rounded-xl'
+                        size={30}
+                     />
+                  </Link>
+                  <MdDeleteForever
+                     className='absolute bg-white/95 top-11 right-2 text-red-700 hover:text-red-800 border rounded-xl cursor-pointer'
                      size={30}
                   />
-               </Link>
-               <MdDeleteForever
-                  className='absolute bg-white/95 top-11 right-2 text-red-700 hover:text-red-800 border rounded-xl cursor-pointer'
-                  size={30}
-               />
-            </div>
+               </div>
+            ))}
          </div>
       </div>
    );
