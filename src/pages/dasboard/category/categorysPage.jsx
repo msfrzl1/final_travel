6b5122c7375b4250de14fec0d89d1178a672415f
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-29 00:25:40 +0700

    added data with api categories

--- src/pages/dasboard/category/categorysPage.jsx
+++ src/pages/dasboard/category/categorysPage.jsx
@@ -1,9 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
 import { BiEdit } from 'react-icons/bi';
 import { MdDeleteForever, MdOutlineLibraryAdd } from 'react-icons/md';
 import { RiSettings4Line } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
+import useGetData from '../../../hooks/isGetData';
 
 export default function CategoryPage() {
+   const [categorys, setCategorys] = useState([]);
+   const { getData } = useGetData();
+
+   useEffect(() => {
+      getData('categories', setCategorys);
+   }, []);
    return (
       <div>
          <div className='flex-col text-center md:flex md:flex-row items-center justify-between pb-2 mb-3 border-b-2'>
@@ -26,26 +35,31 @@ export default function CategoryPage() {
             </Link>
          </div>
          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-            <div className='relative border m-2 rounded-lg overflow-hidden shadow-[0_0_5px_0] transition-transform duration-300 transform hover:scale-[1.01]'>
-               <img
-                  src=''
-                  alt=''
-                  className='w-full h-40 object-cover'
-               />
-               <div className='absolute bottom-0 left-0 right-0 bg-black text-white px-2 py-1'>
-                  <h1 className='text-xs font-semibold'>asa</h1>
-               </div>
-               <Link to=''>
-                  <BiEdit
-                     className='absolute bg-white/95 top-2 right-2 text-indigo-700 hover:text-indigo-800 border rounded-xl'
+            {categorys.map((category) => (
+               <div
+                  key={category.id}
+                  className='relative border m-2 rounded-lg overflow-hidden shadow-[0_0_5px_0] transition-transform duration-300 transform hover:scale-[1.01]'
+               >
+                  <img
+                     src={category.imageUrl}
+                     alt={category.name}
+                     className='w-full h-40 object-cover'
+                  />
+                  <div className='absolute bottom-0 left-0 right-0 bg-black text-white px-2 py-1'>
+                     <h1 className='text-xs font-semibold'>{category.name}</h1>
+                  </div>
+                  <Link to=''>
+                     <BiEdit
+                        className='absolute bg-white/95 top-2 right-2 text-indigo-700 hover:text-indigo-800 border rounded-xl'
+                        size={30}
+                     />
+                  </Link>
+                  <MdDeleteForever
+                     className='absolute bg-white/95 top-11 right-2 text-red-700 hover:text-red-800 border rounded-xl cursor-pointer'
                      size={30}
                   />
-               </Link>
-               <MdDeleteForever
-                  className='absolute bg-white/95 top-11 right-2 text-red-700 hover:text-red-800 border rounded-xl cursor-pointer'
-                  size={30}
-               />
-            </div>
+               </div>
+            ))}
          </div>
       </div>
    );
