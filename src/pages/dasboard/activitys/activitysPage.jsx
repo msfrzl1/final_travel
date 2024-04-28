d1680ac28b42eebfdf194dbcf1a9911dfd8819aa
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-29 00:32:05 +0700

    added data with api activities

--- src/pages/dasboard/activitys/activitysPage.jsx
+++ src/pages/dasboard/activitys/activitysPage.jsx
@@ -1,9 +1,19 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
 import { BiEdit } from 'react-icons/bi';
 import { MdDeleteForever, MdOutlineLibraryAdd } from 'react-icons/md';
 import { RiSettings4Line } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
+import useGetData from '../../../hooks/isGetData';
 
 export default function ActivityPage() {
+   const [activitys, setActivitys] = useState([]);
+   const { getData } = useGetData();
+
+   useEffect(() => {
+      getData('activities', setActivitys);
+   }, []);
+
    return (
       <div>
          <div className='flex-col text-center md:flex md:flex-row items-center justify-between pb-2 mb-3 border-b-2'>
@@ -26,26 +36,31 @@ export default function ActivityPage() {
             </Link>
          </div>
          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-            <div className='relative border m-2 rounded-lg overflow-hidden shadow-[0_0_5px_0] transition-transform duration-300 transform hover:scale-[1.01]'>
-               <img
-                  src=''
-                  alt=''
-                  className='w-full h-40 object-cover'
-               />
-               <div className='absolute bottom-0 left-0 right-0 bg-black text-white px-2 py-1'>
-                  <h1 className='text-xs font-semibold'>asa</h1>
-               </div>
-               <Link to=''>
-                  <BiEdit
-                     className='absolute bg-white/95 top-2 right-2 text-indigo-700 hover:text-indigo-800 border rounded-xl'
+            {activitys.map((activity) => (
+               <div
+                  key={activity.id}
+                  className='relative border m-2 rounded-lg overflow-hidden shadow-[0_0_5px_0] transition-transform duration-300 transform hover:scale-[1.01]'
+               >
+                  <img
+                     src={activity.imageUrls[0]}
+                     alt={activity.title}
+                     className='w-full h-40 object-cover'
+                  />
+                  <div className='absolute bottom-0 left-0 right-0 bg-black text-white px-2 py-1'>
+                     <h1 className='text-xs font-semibold'>{activity.title}</h1>
+                  </div>
+                  <Link to=''>
+                     <BiEdit
+                        className='absolute bg-white/95 top-2 right-2 text-indigo-700 hover:text-indigo-800 border rounded-xl'
+                        size={30}
+                     />
+                  </Link>
+                  <MdDeleteForever
+                     className='absolute bg-white/95 top-11 right-2 text-red-700 hover:text-red-800 border rounded-xl cursor-pointer'
                      size={30}
                   />
-               </Link>
-               <MdDeleteForever
-                  className='absolute bg-white/95 top-11 right-2 text-red-700 hover:text-red-800 border rounded-xl cursor-pointer'
-                  size={30}
-               />
-            </div>
+               </div>
+            ))}
          </div>
       </div>
    );
