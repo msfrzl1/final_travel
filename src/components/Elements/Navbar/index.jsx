c05364b7e94b44166b92c0fe5a0dc20f4fec77b7
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-28 23:31:49 +0700

    change style navbar

--- src/components/Elements/Navbar/index.jsx
+++ src/components/Elements/Navbar/index.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
 
    return (
       <nav className='bg-slate-100 py-2 fixed w-full shadow-[0_0_3px_0] z-[1000]'>
-         <div className='max-w-7xl mx-auto px-3 sm:px-6 lg:px-8'>
+         <div className='max-w-6xl mx-auto px-3'>
             <div className='flex items-center justify-between'>
                <div className='text-xl font-bold font-mono tracking-widest'>
                   <div>INDIGO</div>
