74dcee4ed2de828c0b15b85503ef41719ecc56d8
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-28 22:13:24 +0700

    create user page

--- src/components/Routes/route.jsx
+++ src/components/Routes/route.jsx
@@ -1,3 +1,4 @@
+import UserPage from '../../pages/dasboard/users/usersPage';
 import LoginPage from '../../pages/login';
 import RegisterPage from '../../pages/register';
 import LayoutDasboard from '../Layouts/LayoutDasboard';
@@ -14,5 +15,11 @@ export const routeList = [
    {
       path: '/dasboard',
       element: <LayoutDasboard />,
+      children: [
+         {
+            path: 'users',
+            element: <UserPage />,
+         },
+      ],
    },
 ];
