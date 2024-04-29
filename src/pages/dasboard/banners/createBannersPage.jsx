509e1911aa8b05949e09c4bd2ca547739bfb7687
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-29 22:20:40 +0700

    added handle upload image and create hooks api create

--- src/pages/dasboard/banners/createBannersPage.jsx
+++ src/pages/dasboard/banners/createBannersPage.jsx
@@ -1,8 +1,26 @@
 import { RiSettings4Line } from 'react-icons/ri';
 import FormInput from '../../../components/Elements/FormInput';
 import Button from '../../../components/Elements/Button';
+import useUpload from '../../../hooks/isUpload';
+import { useState } from 'react';
 
 export default function CreateBannerPage() {
+   const [imageUrl, setImageUrl] = useState(null);
+   const { uploadImage } = useUpload();
+   console.log(imageUrl);
+
+   const handleUpload = async (e) => {
+      const file = e.target.files[0];
+      const formData = new FormData();
+      formData.append('image', file);
+      try {
+         const res = await uploadImage('upload-image', formData);
+         setImageUrl(res.data.url);
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
    return (
       <div className='flex max-w-xl mx-auto pt-8 md:pt-0 pb-12 md:pb-1'>
          <div className='w-full border px-3 pb-3 pt-3 rounded-md shadow-[0_0_15px_0] overflow-hidden'>
@@ -28,6 +46,7 @@ export default function CreateBannerPage() {
                      placeholder={'Nama lengkap / Nama Anda'}
                   />
                   <FormInput
+                     onChange={handleUpload}
                      htmlFor={'imageUrl'}
                      title={'Upload Gambar'}
                      type={'file'}
