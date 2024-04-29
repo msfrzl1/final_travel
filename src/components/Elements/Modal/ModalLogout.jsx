38360a4df395748a25b597d1697c4c37b2ec6a42
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-30 00:24:23 +0700

    create modal logout

---
+++ src/components/Elements/Modal/ModalLogout.jsx
@@ -0,0 +1,45 @@
+import { useDispatch } from 'react-redux';
+import { closeModal } from '../../../features/modalSlice';
+
+export default function Modal() {
+   const dispatch = useDispatch();
+   return (
+      <div
+         className='modal fade show'
+         style={{ display: 'block' }}
+         tabIndex='-1'
+      >
+         <div className='modal-dialog modal-dialog-centered'>
+            <div className='modal-content'>
+               <div className='modal-header'>
+                  <h5 className='modal-title'>Konfirmasi</h5>
+                  <button
+                     type='button'
+                     className='btn-close'
+                     aria-label='Close'
+                     onClick={() => dispatch(closeModal())}
+                  ></button>
+               </div>
+               <div className='modal-body'>
+                  <p>Apakah Anda yakin ingin keluar?</p>
+               </div>
+               <div className='modal-footer'>
+                  <button
+                     type='button'
+                     className='btn btn-danger'
+                  >
+                     Confirm
+                  </button>
+                  <button
+                     type='button'
+                     className='btn btn-secondary'
+                     onClick={() => dispatch(closeModal())}
+                  >
+                     Cancel
+                  </button>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+}
