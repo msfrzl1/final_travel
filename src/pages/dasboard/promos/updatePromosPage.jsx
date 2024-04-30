e6f1deb649cc13a4342c0892d25f28ac35804aa3
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-30 23:30:06 +0700

    added value in from update promo

--- src/pages/dasboard/promos/updatePromosPage.jsx
+++ src/pages/dasboard/promos/updatePromosPage.jsx
@@ -1,8 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { RiSettings4Line } from 'react-icons/ri';
 import FormInput from '../../../components/Elements/FormInput';
 import Button from '../../../components/Elements/Button';
+import { useParams } from 'react-router-dom';
+import useGetData from '../../../hooks/isGetData';
+import { useEffect, useState } from 'react';
 
 export default function UpdatePromosPage() {
+   const [promo, setPromo] = useState([]);
+   const { getData } = useGetData();
+   const { id } = useParams();
+
+   useEffect(() => {
+      getData(`promo/${id}`, setPromo);
+   }, []);
+
    return (
       <div className='flex max-w-xl mx-auto pt-3 pb-1'>
          <div className='w-full border px-3 pb-3 pt-3 rounded-md shadow-[0_0_15px_0] overflow-hidden'>
@@ -24,11 +36,13 @@ export default function UpdatePromosPage() {
                   <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
                      <div className='w-full'>
                         <FormInput
+                           defaultValue={promo.title}
                            htmlFor={'title'}
                            title={'Nama'}
                            name={'title'}
                         />
                         <FormInput
+                           defaultValue={promo.description}
                            htmlFor={'description'}
                            title={'Deskripsi'}
                            name={'description'}
@@ -42,22 +56,26 @@ export default function UpdatePromosPage() {
                      </div>
                      <div className='w-full'>
                         <FormInput
+                           defaultValue={promo.terms_condition}
                            htmlFor={'terms_condition'}
                            title={'Syarat Kondisi'}
                            name={'terms_condition'}
                         />
                         <FormInput
+                           defaultValue={promo.promo_code}
                            htmlFor={'promo_code'}
                            title={'Kode Promo'}
                            name={'promo_code'}
                         />
                         <FormInput
+                           defaultValue={promo.minimum_claim_price}
                            htmlFor={'minimum_claim_price'}
                            title={'Klaim Harga Minimum'}
                            type={'number'}
                            name={'minimum_claim_price'}
                         />
                         <FormInput
+                           defaultValue={promo.promo_discount_price}
                            htmlFor={'promo_discount_price'}
                            title={'Diskon Harga Promo'}
                            type={'number'}
