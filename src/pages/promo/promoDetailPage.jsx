f851f9b87cc16df1ca9e0d9cdd8409bf3ea86cf9
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-30 13:58:34 +0700

    added data promo detail by id

--- src/pages/promo/promoDetailPage.jsx
+++ src/pages/promo/promoDetailPage.jsx
@@ -1,7 +1,18 @@
-import { Link } from 'react-router-dom';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Link, useParams } from 'react-router-dom';
 import Layout from '../../components/Layouts/Layout';
+import useGetData from '../../hooks/isGetData';
+import { useEffect, useState } from 'react';
 
 export default function PromoDetailUser() {
+   const [promo, setPromo] = useState([]);
+   const { id } = useParams();
+   console.log(promo);
+   const { getData } = useGetData();
+
+   useEffect(() => {
+      getData(`promo/${id}`, setPromo);
+   }, []);
    return (
       <Layout>
          <div className='flex gap-2 text-xs font-semibold mb-3 px-3 pt-20'>
@@ -19,35 +30,35 @@ export default function PromoDetailUser() {
                promo-users
             </Link>
             <div>/</div>
-            <div>asas</div>
+            <div>{promo.title}</div>
          </div>
          <div className='flex flex-col md:flex-row gap-4 px-4 pb-5'>
             <div className='md:w-1/2'>
                <img
-                  src=''
-                  alt=''
+                  src={promo.imageUrl}
+                  alt={promo.title}
                   className='w-full h-96 object-cover rounded-lg shadow-[0_0_3px_0]'
                />
             </div>
             <div className='flex-1 bg-white rounded-lg shadow-[0_0_3px_0] p-4'>
-               <h1 className='text-2xl font-semibold mb-2'>sasa</h1>
+               <h1 className='text-2xl font-semibold mb-2'>{promo.title}</h1>
                <div className='mb-2 text-sm'>
                   <span className='font-semibold'>Minimum Claim Price:</span>
-                  <span className='ml-2 text-indigo-600'>sasa</span>
+                  <span className='ml-2 text-indigo-600'>{promo.minimum_claim_price}</span>
                </div>
                <div className='mb-2 text-sm'>
                   <span className='font-semibold'>Discount Price:</span>
-                  <span className='ml-2 text-indigo-600'>sasa</span>
+                  <span className='ml-2 text-indigo-600'>{promo.promo_discount_price}</span>
                </div>
                <div className='mb-4 text-sm'>
                   <span className='font-semibold'>Promo Code:</span>
-                  <span className='ml-2 text-indigo-500'>asasa</span>
+                  <span className='ml-2 text-indigo-500'>{promo.promo_code}</span>
                </div>
                <div className='max-w-4xl border rounded p-4 bg-slate-200'>
                   <div className='text-lg font-bold mb-1'>Deskripsi</div>
-                  <div className='text-sm mb-4'>sasa</div>
+                  <div className='text-sm mb-4'>{promo.description}</div>
                   <div className='text-lg font-bold mb-1'>Syarat Kondisi</div>
-                  <p className='text-sm'>sasa</p>
+                  <p className='text-sm'>{promo.terms_condition}</p>
                </div>
             </div>
          </div>
