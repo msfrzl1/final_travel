450a4564a003cf58c8ca2849a90c6c773867738a
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-04-30 22:08:22 +0700

    addded modal role in store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { modalLogoutSlice } from './features/modalLogoutSlice';
+import { modalRoleSlice } from './features/modalRoleSlice';
 
 export const store = configureStore({
    reducer: {
       modalLogout: modalLogoutSlice.reducer,
+      modalRole: modalRoleSlice.reducer,
    },
 });
 
