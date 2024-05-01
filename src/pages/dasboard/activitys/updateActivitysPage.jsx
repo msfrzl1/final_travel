fab788e29f1980dc8803c55f8495f52571277bcf
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-05-01 21:36:03 +0700

    create from update activity page

--- src/pages/dasboard/activitys/updateActivitysPage.jsx
+++ src/pages/dasboard/activitys/updateActivitysPage.jsx
@@ -1,7 +1,136 @@
+import { MdDeleteForever } from 'react-icons/md';
+import { RiSettings4Line } from 'react-icons/ri';
+import FormInput from '../../../components/Elements/FormInput';
+import Button from '../../../components/Elements/Button';
+
 export default function UpdateActivitysPage() {
    return (
-      <div>
-         <h1>UpdateActivitysPage</h1>
+      <div className='flex max-w-4xl mx-auto mt-3'>
+         <div className='w-full border px-3 pb-3 pt-3 rounded-md shadow-[0_0_15px_0] overflow-hidden'>
+            <div className='flex items-center gap-2 mb-1'>
+               <RiSettings4Line
+                  size={25}
+                  className='text-red-500'
+               />
+               <h1 className='text-xl font-black font-mono'>Pengaturan Activity</h1>
+            </div>
+            <div className='border-b-2 mb-3'></div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mb-3 gap-3'>
+               <div className='relative w-full aspect-w-3 aspect-h-4'>
+                  <img
+                     src={''}
+                     alt='image'
+                     className='w-full h-full object-cover shadow-[0_0_5px_0] rounded-md'
+                  />
+                  <MdDeleteForever
+                     className='absolute bg-white/95 top-2 right-2 text-red-700 hover:text-red-800 border rounded-xl cursor-pointer'
+                     size={25}
+                  />
+               </div>
+            </div>
+            <div className='w-full border px-3 pb-3 rounded-md shadow-[0_0_5px_0]'>
+               <form>
+                  <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
+                     <div className='w-full'>
+                        <div className='flex flex-col my-2'>
+                           <label
+                              htmlFor='categoryId'
+                              className='text-sm font-bold mb-1'
+                           >
+                              Category ID
+                           </label>
+                           <select
+                              name='categoryId'
+                              id='categoryId'
+                              className='border rounded-md py-[0.2rem]'
+                           >
+                              <option
+                                 value=''
+                                 key=''
+                                 selected=''
+                              >
+                                 name
+                              </option>
+                           </select>
+                        </div>
+                        <FormInput
+                           htmlFor={'title'}
+                           title={'Judul Activity'}
+                           name={'title'}
+                        />
+                        <FormInput
+                           htmlFor={'description'}
+                           title={'Deskripsi'}
+                           name={'description'}
+                        />
+                        <FormInput
+                           htmlFor={'imageUrls'}
+                           title={'Upload Gambar'}
+                           type={'file'}
+                           name={'imageUrls'}
+                        />
+                        <FormInput
+                           htmlFor={'price'}
+                           title={'Harga'}
+                           type={'number'}
+                           name={'price'}
+                        />
+                        <FormInput
+                           htmlFor={'price_discount'}
+                           title={'Harga Diskon'}
+                           type={'number'}
+                           name={'price_discount'}
+                        />
+                        <FormInput
+                           htmlFor={'rating'}
+                           title={'Penilaian'}
+                           type={'number'}
+                           name={'rating'}
+                        />
+                     </div>
+                     <div className='w-full mt-[0.5rem]'>
+                        <FormInput
+                           htmlFor={'total_reviews'}
+                           title={'Total Ulasan'}
+                           type={'number'}
+                           name={'total_reviews'}
+                        />
+                        <FormInput
+                           htmlFor={'facilities'}
+                           title={'Fasilitas'}
+                           name={'facilities'}
+                        />
+                        <FormInput
+                           htmlFor={'address'}
+                           title={'Alamat'}
+                           name={'address'}
+                        />
+                        <FormInput
+                           htmlFor={'province'}
+                           title={'Provinsi'}
+                           name={'province'}
+                        />
+                        <FormInput
+                           htmlFor={'city'}
+                           title={'Kota'}
+                           name={'city'}
+                        />
+                        <FormInput
+                           htmlFor={'location_maps'}
+                           title={'Lokasi Maps'}
+                           name={'location_maps'}
+                        />
+                     </div>
+                  </div>
+                  <Button
+                     className='bg-indigo-600 hover:bg-indigo-700 w-full mt-1'
+                     type='submit'
+                  >
+                     Edit Activity
+                  </Button>
+               </form>
+            </div>
+         </div>
       </div>
    );
 }
