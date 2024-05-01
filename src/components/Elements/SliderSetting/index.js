2ffee29fc4cb16fec3247927ce3ee5afc2f50b6e
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-05-01 15:52:29 +0700

    install react slick carousel

---
+++ src/components/Elements/SliderSetting/index.js
@@ -0,0 +1,21 @@
+export const sliderSettings1 = {
+   className: 'center',
+   infinite: true,
+   centerPadding: '60px',
+   slidesToShow: 4,
+   swipeToSlide: true,
+   responsive: [
+      {
+         breakpoint: 1024,
+         settings: {
+            slidesToShow: 2,
+         },
+      },
+      {
+         breakpoint: 600,
+         settings: {
+            slidesToShow: 1,
+         },
+      },
+   ],
+};
